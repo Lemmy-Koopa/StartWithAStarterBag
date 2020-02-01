21b7264197cbcf119195705250dcc46b4c5259b6
charlie brannan <charliebrannan154@gmail.com>
2020-02-01 11:07:48 +1100

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # StartWithAStarterBag
+StartWithAStarterBag adds a starter bag so you no longer need Calamity for one.
+
+(Version Changelog)
+
+0.1 - The mod is relased 
+0.2 - Removed the ExampleSword and Added Recall potions
+0.3 - Test Version 
+0.3.1 - Icon Hopefully added
+0.3.2 - Icon Added
+0.4 - Changed Some of the drop chances for items and added the Copper BroadSword
+1.0 - Cleaned up all code and added config
--- StartWithAStarterBag.cs
+++ StartWithAStarterBag.cs
@@ -1,11 +1,65 @@
+using GoodProLib.GData;
+using StartWithAStarterBag.Items;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 
 namespace StartWithAStarterBag
 {
 	public class StartWithAStarterBag : Mod
 	{
-		public StartWithAStarterBag()
+	}
+
+	public class StartWithAStarterBagPlayer : ModPlayer
+	{
+		public override void SetupStartInventory(IList<Item> items, bool mediumcoreDeath)
 		{
+			Item item = new Item();
+			item.SetDefaults(ModContent.ItemType<StarterBag>());
+			item.stack = 1;
+			items.Add(item);
+		}
+	}
+
+	public class SWSBConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ServerSide;
+
+		[Label("Starter bag item list")]
+		[Tooltip("Every item in this list will be in the starter bag when opened")]
+		public Dictionary<ItemDefinition, int> StarterBagItems = new Dictionary<ItemDefinition, int>()
+		{
+			[new ItemDefinition(ItemID.MiningPotion)] = Main.rand.Next(1, 3),
+			[new ItemDefinition(ItemID.SpelunkerPotion)] = 1,
+			[new ItemDefinition(ItemID.WormholePotion)] = Main.rand.Next(2, 6),
+			[new ItemDefinition(ItemID.IronskinPotion)] = Main.rand.Next(1, 3),
+			[new ItemDefinition(ItemID.LesserHealingPotion)] = Main.rand.Next(3, 5),
+			[new ItemDefinition(ItemID.Torch)] = Main.rand.Next(25, 60),
+			[new ItemDefinition(ItemID.SlimeStaff)] = 1,
+			[new ItemDefinition(ItemID.AmethystStaff)] = 1,
+			[new ItemDefinition(ItemID.CopperBow)] = 1,
+			[new ItemDefinition(ItemID.CopperBroadsword)] = 1,
+			[new ItemDefinition(ItemID.WoodenArrow)] = Main.rand.Next(100, 200),
+			[new ItemDefinition(ItemID.Star)] = Main.rand.Next(3, 7),
+			[new ItemDefinition(ItemID.Wood)] = Main.rand.Next(20, 100),
+			[new ItemDefinition(ItemID.ThrowingKnife)] = 200,
+			[new ItemDefinition(ItemID.RecallPotion)] = 5,
+		};
+
+		public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
+		{
+			if (PlayerData.IsPlayerServerHost(Main.LocalPlayer, out _))
+			{
+				message = "You are the server host, config changed.";
+				return true;
+			}
+			else
+			{
+				message = "You are not the server host, config not changed.";
+				return false;
+			}
 		}
 	}
 }
